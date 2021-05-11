c2d51cf7f898ef406a655681d9d0ec8dae838b2a
Vivian Luxon <arterinv@gmail.com>
2021-05-11 15:06:28 +1200

    renamed

--- 1-functions-and-arrow-functions/1-functions-and-arrow-functions-from-class.js
+++ 1-functions-and-arrow-functions/1-functions-and-arrow-functions-from-class.js
@@ -2,10 +2,12 @@
 const square1 = function (n) { return n * n };
 console.log(square1(5));
 
+// ---------------------------------------------------------------
 // Function declaration.
 function square2 (n) { return n * n };
 console.log(square2(10));
 
+// ---------------------------------------------------------------
 // Arrow function.
 const square3 = n => n * n;
 console.log(square3(2));
